f60fcce967f0faf7c7858947da4ec6e975be64fa
Christophe Willemsen <willemsen.christophe@gmail.com>
2021-01-16 21:51:58 +0100

    initial commit

---
+++ spring-cloud-function/src/main/java/com/cwillemsen/cloudnative/springcloudfunction/SpringCloudFunctionApplication.java
@@ -0,0 +1,27 @@
+package com.cwillemsen.cloudnative.springcloudfunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+@SpringBootApplication
+public class SpringCloudFunctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudFunctionApplication.class, args);
+	}
+
+	@Bean
+	public Function<String, String> uppercase() {
+		return String::toUpperCase;
+	}
+
+	@Bean
+	public Consumer<String> consume() {
+		return str -> System.out.println("Received " + str);
+	}
+
+}
